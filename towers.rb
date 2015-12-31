6be244e2df7b1c50c5f0a8a8c29c0d13abb745fc
Julia Herron <julia.herron@gmail.com>
2015-12-31 14:49:11 -0800

    it doesn't really work, but i think i know where i went wrong. will come back later

--- towers.rb
+++ towers.rb
@@ -45,19 +45,16 @@ class TowerofHanoi
 
     print gameboard
     return gameboard
-
   end
 
 
   def user_input
     # TODO: make this validation correct, maybe use regex?
     input_array = nil
-    until input_array.is_a?(Array) # && input.length == 2
-      puts "\nEnter move >"
-      input = gets.chomp
-      # `gets` returns a string, need to chop it into an array
-      input_array = input[1..-2].split(',').collect! {|n| n.to_i}
-    end
+    puts "\nEnter move >"
+    input = gets.chomp
+    # `gets` returns a string, need to chop it into an array
+    input_array = input[1..-2].split(',').collect! {|n| n.to_i}
     return input_array
   end
 
@@ -73,10 +70,9 @@ class TowerofHanoi
     from_column = input_array[0]-1 # [1] column, really 0
     # find top-most disk in from_column
     row = 0
-    disk_to_move = false
-    until disk_to_move
+    @height.times do
       if gameboard[row][from_column] == 0
-        first_row +=1
+        row +=1
       else
         disk_to_move = gameboard[row][from_column]
         # replace disk with 0
@@ -91,7 +87,7 @@ class TowerofHanoi
     to_column = input_array[1]-1  # [3] column, really 2
     # move disk to lowest 0 in to_column
     bottom_row = @height-1
-    @height.times do
+    (@height-1).times do
       if gameboard[bottom_row][to_column] == 0
         gameboard[bottom_row][to_column] = disk_to_move
         break
@@ -104,7 +100,7 @@ class TowerofHanoi
   end
 
 
-  def user_has_won?(gameboard)
+  def user_has_won(gameboard)
     solution = (1..@height).to_a
     row_index = 0
     column_index = 0
@@ -123,10 +119,11 @@ class TowerofHanoi
     print_instructions
     gameboard = make_gameboard(@height)
 
-    input = ""
-    while input != "q"
+    input_array = ""
+    while input_array != "q"
 
       input_array = user_input
+      print input_array
       validate_move
       disk_to_move = remove_disk(input_array, gameboard)
       move_disk_to_new_column(input_array, gameboard, disk_to_move)
